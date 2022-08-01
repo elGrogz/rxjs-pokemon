0a9e80e3af24b961e242ece9efcfaf0e06311f8b
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2022-08-01 22:49:54 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+// import the obervable subect
+import { pokemonWithPower$ } from "./store"
+
+
+
 function App() {
+  // on initial render, subject to the observable and log the initial value 
+  useEffect(() => {
+    // debugger
+    pokemonWithPower$.subscribe(console.log);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
---
+++ src/store.tsx
@@ -0,0 +1,38 @@
+import { BehaviorSubject, map } from "rxjs";
+
+export interface Pokemon {
+    id: number,
+    name: string,
+    type: string[],
+    hp: number,
+    attack: number,
+    defense: number,
+    special_attack: number,
+    special_defense: number,
+    speed: number,
+    power?: number
+}
+
+// create an observable subject
+// behavior subjects require an initial value and emits the current value when subscribed
+const rawPokemon$ = new BehaviorSubject<Pokemon[]>([]);
+
+// 'pipes' the output of rawPokemon  to the rxjs function
+export const pokemonWithPower$ = rawPokemon$.pipe(
+    // takes the list of rawPokemon
+    map(pokemon => {
+        // create a new array of pokemon
+        pokemon.map(p => ({
+            //first spread the array with the initial values
+            ...p,
+            // add a new power attribute
+            power: p.hp + p.attack + p.defense + p.special_attack + p.special_defense
+        }))
+    })
+)
+
+// get the data from the file, get the response as json, then set the next value of the observable as the data
+fetch('/pokemon-data.json')
+    // .then(res => res.json())
+    // .then(data => rawPokemon$.next(data))
+    .then(res => console.log(res.json()))
