b800749c91f3aa345ef7eb04ee00b12249a794a2
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-09-11 14:37:30 +0200

    moved search state into observable hook - issue with useObservableHook function signature

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,58 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import './App.css';
+import React, { useEffect, useState, useMemo } from "react";
+import "./App.css";
 
-// import the obervable subect
-import { PokemonProvider, pokemon$, selected$, deck$ } from "./store"
-import { useObservableState } from "observable-hooks"
+// import the obervable subject
+import { PokemonProvider, usePokemon } from "./store";
+import { useObservableState } from "observable-hooks";
+import { BehaviorSubject, combineLatestWith, map } from "rxjs";
 // import { rawPokemon$ } from "./store"
 
 const Deck = () => {
-    const deck = useObservableState(deck$, []);
+  const { deck$ } = usePokemon();
+  const deck = useObservableState(deck$, []);
 
-    return (
+  return (
+    <div>
+      <h4>Deck</h4>
       <div>
-        <h4>Deck</h4>
-        <div>
-          {deck.map((p) => (
-            <div key={p.id} style={{display: 'flex'}}>
-              <img 
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
-                alt={p.name}/>
-                <div>
-                  {p.name}
-                </div>
-            </div>
-          ))}
-        </div>
+        {deck.map((p) => (
+          <div key={p.id} style={{ display: "flex" }}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
+              alt={p.name}
+            />
+            <div>{p.name}</div>
+          </div>
+        ))}
       </div>
-    )
-}
+    </div>
+  );
+};
 
 const Search = () => {
+  const { pokemon$, selected$ } = usePokemon();
+
   // state to store and update the search term
-  const [search, setSearch] = useState(""); 
+  const search$ = useMemo(() => new BehaviorSubject(""), []);
+  // const [search, setSearch] = useState("");
 
   // state to store and update the full list of pokemon
-  const pokemon = useObservableState(pokemon$, []); 
+  const pokemon = useObservableState(pokemon$, []);
+
+  const [filteredPokemon] = useObservableState(() =>
+    pokemon$.pipe(
+      combineLatestWith(search$),
+      map(([pokemon, search]) => {
+        pokemon.filter((p) =>
+          p.name.toLowerCase().includes(search.toLowerCase())
+        );
+      })
+    )
+  );
 
   // NOT NEEDED WHEN YOU HAVE USEOBSERVABLESTATE
   // run when the component is mounted
-  // useEffect(() => { 
+  // useEffect(() => {
   //   // subscribe to the observable which returns the list of pokemon after it's fetched and then set the
   //   // state to be the full list
   //   const subscription = pokemon$.subscribe(setPokemon);
@@ -47,57 +62,65 @@ const Search = () => {
   // }, []);
 
   // useMemo so filteredPokemon only runs when one of it's dependencies (pokemon or search) changes
-  const filteredPokemon = useMemo(() => {
-    // filter pokemon based on the search and their name
-    return pokemon.filter(p => p.name.toLowerCase().includes(search.toLowerCase()));
-  }, [pokemon, search]);
+  // const filteredPokemon = useMemo(() => {
+  //   // filter pokemon based on the search and their name
+  //   return pokemon.filter((p) =>
+  //     p.name.toLowerCase().includes(search$.value.toLowerCase())
+  //   );
+  // }, [pokemon, search]);
 
   useEffect(() => {
-    console.log("POKEMON: ", pokemon)
-    console.log("SELECTED: ", selected$)
+    console.log("POKEMON: ", pokemon);
+    console.log("SELECTED: ", selected$);
   }, [pokemon]);
-
 
   return (
     <div>
       {/* every changes causes the search term to update, and we display it with valye */}
-      <input type="text" value={search} onChange={(event) => setSearch(event.target.value)}/>
+      <input
+        type="text"
+        value={search$.value}
+        onChange={(event) => search$.next(event.target.value)}
+      />
       <div>
         {/* show the filtered pokemon with a map function */}
         {filteredPokemon.map((p) => (
           <div key={p.name}>
-            <input type="checkbox" 
+            <input
+              type="checkbox"
               // check box if it's selected attribute is true
               checked={p.selected}
-
               //on change, if the selected array includes the rendered pokemon, update the selected array to not include the id
               onChange={() => {
                 if (selected$.value.includes(p.id)) {
-                  selected$.next(selected$.value.filter((id) => id !== p.id))
-                // otherwise if not already selected, 
+                  selected$.next(selected$.value.filter((id) => id !== p.id));
+                  // otherwise if not already selected,
                 } else {
-                  selected$.next([...selected$.value, p.id])
+                  selected$.next([...selected$.value, p.id]);
                 }
-              }}/>
+              }}
+            />
             <strong>{p.name}:</strong> {p.power}
           </div>
         ))}
       </div>
     </div>
-  )
-}
- 
+  );
+};
+
 function App() {
   return (
     <PokemonProvider>
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: "1fr 1fr"
-    }}>
-      <Search/>
-      <Deck/>
-    </div>
-      </PokemonProvider>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+        }}
+      >
+        <Search />
+        <Deck />
+      </div>
+    </PokemonProvider>
   );
 }
 
--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,19 @@
 import { BehaviorSubject, map, combineLatestWith } from "rxjs";
 
-import { createContext, useContext } from 'react'
+import { createContext, useContext } from "react";
 
 export interface Pokemon {
-    id: number;
-    name: string;
-    type: string[];
-    hp: number;
-    attack: number;
-    defense: number;
-    special_attack: number;
-    special_defense: number;
-    speed: number;
-    power?: number;
-    selected?: boolean;
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+  power?: number;
+  selected?: boolean;
 }
 
 // create an observable subject
@@ -22,61 +22,74 @@ const rawPokemon$ = new BehaviorSubject<Pokemon[]>([]);
 
 // 'pipes' the output of rawPokemon  to the rxjs function
 const pokemonWithPower$ = rawPokemon$.pipe(
-    // takes the list of rawPokemon
-    map((pokemon) => 
-        // create a new array of pokemon
-        pokemon.map((p) => (
-            {
-                //first spread the array with the initial values
-                ...p,
-                // add a new power attribute
-                power: p.hp + p.attack + p.defense + p.special_attack + p.special_defense + p.speed,
-            }))
-    )
+  // takes the list of rawPokemon
+  map((pokemon) =>
+    // create a new array of pokemon
+    pokemon.map((p) => ({
+      //first spread the array with the initial values
+      ...p,
+      // add a new power attribute
+      power:
+        p.hp +
+        p.attack +
+        p.defense +
+        p.special_attack +
+        p.special_defense +
+        p.speed,
+    }))
+  )
 );
 
 // keeps a list of the id's that we've selected
 export const selected$ = new BehaviorSubject<number[]>([]);
 
 export const pokemon$ = pokemonWithPower$.pipe(
-    // add the selected observable to the pokemon one
-    // when anything here changes, pokemon$ is recalculated
-    combineLatestWith(selected$),
-    //create a new output using both inputs (does it just know to use pokemonWithPower and selected as argument 1 and 2?)
-    map(([pokemon, selected]) => 
-        // new array output with map that has true or false for if the pokemon (by id) is in the selected array
-      pokemon.map((p) => ({
-        ...p,
-        selected: selected.includes(p.id),
-      }))
-    )
-)
+  // add the selected observable to the pokemon one
+  // when anything here changes, pokemon$ is recalculated
+  combineLatestWith(selected$),
+  //create a new output using both inputs (does it just know to use pokemonWithPower and selected as argument 1 and 2?)
+  map(([pokemon, selected]) =>
+    // new array output with map that has true or false for if the pokemon (by id) is in the selected array
+    pokemon.map((p) => ({
+      ...p,
+      selected: selected.includes(p.id),
+    }))
+  )
+);
 
 // observable to include any pokemon that are selected in the deck\
 // runs anytime pokemon$ changes
 export const deck$ = pokemon$.pipe(
-    map((pokemon) => pokemon.filter((p) => p.selected))
-)
-
+  map((pokemon) => pokemon.filter((p) => p.selected))
+);
 
 // get the data from the file, get the response as json, then set the next value of the observable as the data
-fetch('/pokemon-data.json')
-    .then(res => res.json())
-    .then(data => rawPokemon$.next(data))
+fetch("/pokemon-data.json")
+  .then((res) => res.json())
+  .then((data) => rawPokemon$.next(data));
 
 const PokemonContext = createContext({
-    pokemon$,
-    selected$,
-    deck$,
+  pokemon$,
+  selected$,
+  deck$,
 });
 
-export const PokemonProvider: React.FunctionComponent = ({ children }) => (
-    <PokemonContext.Provider value={{
-        pokemon$,
-        selected$,
-        deck$
+export const usePokemon = () => useContext(PokemonContext);
+
+type PokemonProps = {
+  children: React.ReactNode; // 👈️ type children
+};
+
+export const PokemonProvider: React.FunctionComponent<PokemonProps> = ({
+  children,
+}) => (
+  <PokemonContext.Provider
+    value={{
+      pokemon$,
+      selected$,
+      deck$,
     }}
-    >
-        {children}
-    </PokemonContext.Provider>
-)
+  >
+    {children}
+  </PokemonContext.Provider>
+);
